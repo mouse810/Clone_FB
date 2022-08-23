ff0528d3b36c2e0b9577dc9642b7e36118c7facd
mouse810 <mousegamingv2.0@gmail.com>
2022-08-23 19:36:36 +0530

    sada2

--- src/Layout/Feeds/Post/Post.js
+++ src/Layout/Feeds/Post/Post.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Avatar } from '@mui/material'
+import { ThumbUp, Share, Message } from '@mui/icons-material'
 import './Post.css'
 
 const Post = ({ profilePic, image, userName, timeStamp, message }) => {
     return (
         <div className='post'>
             <div className="post__top">
-                <Avatar src={profilePic} className='post__avatar' />
+                <Avatar src={profilePic, message} className='post__avatar' />
                 <div className="post__topInfo">
                     <h3>{userName}</h3>
                     <p>TimStamp...</p>
@@ -15,8 +16,28 @@ const Post = ({ profilePic, image, userName, timeStamp, message }) => {
             <div className="post__bottom">
                 <p>{message}</p>
             </div>
-            <div className="post__image" style={{overflow:"hidden"}}>
+            <div className="post__image" style={{ overflow: "hidden" }}>
                 <img src={image} alt="" />
+            </div>
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUp />
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <Message />
+                    <p>Comment</p>
+                </div>
+                <div className="post__option">
+                    <Share />
+                    <p>Share</p>
+                </div>
+                <div className="post__option">
+
+                </div>
+                <div className="post__option">
+
+                </div>
             </div>
         </div>
     )
