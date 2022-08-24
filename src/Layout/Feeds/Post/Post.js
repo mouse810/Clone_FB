27fbe579fe1c963c27c9736c72868f28af915301
mouse810 <mousegamingv2.0@gmail.com>
2022-08-24 10:54:13 +0530

    ddf

--- src/Layout/Feeds/Post/Post.js
+++ src/Layout/Feeds/Post/Post.js
@@ -7,7 +7,7 @@ const Post = ({ profilePic, image, userName, timeStamp, message }) => {
     return (
         <div className='post'>
             <div className="post__top">
-                <Avatar src={profilePic, message} className='post__avatar' />
+                <Avatar src={profilePic} className='post__avatar' />
                 <div className="post__topInfo">
                     <h3>{userName}</h3>
                     <p>TimStamp...</p>
